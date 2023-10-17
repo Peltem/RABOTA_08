4508285440cbb39d9c33329e8c2aa94a17143a3d
student <gnil47311@gmail.com>
2023-10-17 09:21:26 +0700

    17.10

--- RABOTA_08/AddWindow.axaml.cs
+++ RABOTA_08/AddWindow.axaml.cs
@@ -21,7 +21,7 @@ public partial class AddWindow : Window
         string _connString = "server=10.10.1.24;database=pro1_8;User Id=user_01;password=user01pro";
         List<Application> _aplications;
         MySqlConnection _connection;
-        string sql = "INSERT Заявка SET ОборудованиеIDs = @Equip, СерийныйНомер = @SerialN, ОписаниеПроблемы = @Description  WHERE ID_Клиента = @Id;";
+        string sql = "INSERT INTO Заявка (ОборудованиеIDs, СерийныйНомер, ОписаниеПроблемы, ТипОборудование, ПриоритетIDs, КлиентIDs) VALUES (@Equip, @SerialN, @Description, @Type, @Priority, @Client);";
         _aplications = new List<Application>();
         _connection = new MySqlConnection(_connString);
         _connection.Open();
@@ -31,5 +31,19 @@ public partial class AddWindow : Window
         command.Parameters.Add("@Equip", MySqlDbType.Int32);
         command.Parameters["@AddName"].Value = Equip.Text;
         command.Parameters.Add("@SerialN", MySqlDbType.Int32);
-        command.Parameters["@AddNumber"].Value = SerialN.Text;
+        command.Parameters["@SerialN"].Value = SerialN.Text;
+        command.Parameters.Add("@Description", MySqlDbType.String);
+        command.Parameters["@Description"].Value = Description.Text;
+        command.Parameters.Add("@Type", MySqlDbType.Int32);
+        command.Parameters["@Type"].Value = Type.Text;
+        command.Parameters.Add("@Prioirity", MySqlDbType.Int32);
+        command.Parameters["@Priority"].Value = Priority.Text;
+        command.Parameters.Add("@Client", MySqlDbType.Int32);
+        command.Parameters["@Client"].Value = Client.Text;
+        command.ExecuteNonQuery();
+        this.Close(true);
+        Result = true;
+    }
+    public bool Result { get; private set; }
 }
+
